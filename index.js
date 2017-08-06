5a3517b824a607cbb4fa7e9fa7475ce6fd680775
zchhaenngg <1076968967@qq.com>
2017-08-06 23:14:13 +0800

    npm run build

---
+++ index.js
@@ -0,0 +1,13 @@
+class a{
+    constructor(aa,bb){
+        super();
+        this.aa = aa;
+        this.bb = bb;
+    }
+    
+    test(){
+        console.console.log('====================================');
+        console.log("log 111");
+        console.log('====================================');
+    }
+}
